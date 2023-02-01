cc6bee8c167364b211dd729ca9449ae93d736883
Привoзгрин Егoр <privozgrinegor@yandex.ru>
2023-02-01 20:50:03 +0300

    Добавьте файлы проекта.

---
+++ dz/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace dz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вводи первое число:");
+            string? a = Console.ReadLine();
+            Console.WriteLine("Вводи первое число:");
+            string? b = Console.ReadLine();
+            Console.WriteLine("Вводи первое число:");
+            string? c = Console.ReadLine();
+            Console.WriteLine("Вводи первое число:");
+            string? d = Console.ReadLine();
+            Console.WriteLine($"{a} {b} {c} {d}");
+        }
+    }
+}
