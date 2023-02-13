cfe750d255addb8e261c1ac5b9c749719308007a
EgorPrivzogrin <98107069+EgorPrivzogrin@users.noreply.github.com>
2023-02-13 15:18:19 +0300

    Update Program.cs

--- dz/Program.cs
+++ dz/Program.cs
@@ -4,13 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Вводи первое число:");
+            Console.WriteLine("Ввeди число:");
             string? a = Console.ReadLine();
-            Console.WriteLine("Вводи первое число:");
+            Console.WriteLine("Ввeди число:");
             string? b = Console.ReadLine();
-            Console.WriteLine("Вводи первое число:");
+            Console.WriteLine("Ввeди число:");
             string? c = Console.ReadLine();
-            Console.WriteLine("Вводи первое число:");
+            Console.WriteLine("Ввeди число:");
             string? d = Console.ReadLine();
             Console.WriteLine($"{a} {b} {c} {d}");
         }
